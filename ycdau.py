897ae2e925f8974a3f56402164cd8511c358d7ae
GitOffMyLAN <jackpsthefirst@gmail.com>
2018-07-30 14:58:59 +1000

    added a internet mode to change the speed of downloading

--- ycdau.py
+++ ycdau.py
@@ -7,10 +7,10 @@ parser.add_option("-c", "--config", dest="filename",
                   help="File that has the channel and download info", metavar="FILE")
 parser.add_option("-a", "--add", dest="array",
                   help="To add a new youtube channel and any other info", metavar="ARRAY")
-parser.add_option("-s", "--init", dest="init",
-                  help="To initalize the program with configs", metavar="")
 parser.add_option("-i", "--index", dest="index",
                   help="to set the index to which to read the file from, say to skip the entry 10 in your list then -i 10", metavar="")
+parser.add_option("-s", "--spec-internet", dest="downloadSpeed",
+                 help="To set the script to download at a set download speed")
 (options, args) = parser.parse_args()
 #finds the config file
 if options.filename != None:
@@ -27,6 +27,8 @@ if options.filename != None:
         i = 0
     while i < len(config_array):
         print(i)
+        if options.downloadSpeed != None:
+         command = "youtube-dl -r " + option.downloadSpeed + " -o '" + str(config_array[i][0]).replace('"', '') + "/%(title)s.%(ext)s'" + str(config_array[i][2]).replace('"', '') + str(config_array[i][1]).replace('"', '') 
         command = "youtube-dl -o '" + str(config_array[i][0]).replace('"', '') + "/%(title)s.%(ext)s'" + str(config_array[i][2]).replace('"', '') + str(config_array[i][1]).replace('"', '')
         print(command)
         os.system(command)
